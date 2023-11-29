4bc83f51b71cc2ccf7d8b5b35a0ae1becb024bd6
euge <euge.luque06@gmail.com>
2023-11-29 14:30:27 -0300

    subir

---
+++ src/screens/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SearchBar = () => {
+  // Lógica del buscador
+
+  return (
+    <View>
+      <Text>Buscador</Text>
+      {/* Puedes agregar aquí la lógica del buscador */}
+    </View>
+  );
+};
+
+export default SearchBar;
